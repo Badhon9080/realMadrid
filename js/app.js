7e689bfc076b0b73110bc951e3cdc18fd821ba01
BADHAN SEN <96924254+Badhon9080@users.noreply.github.com>
2023-02-27 16:27:42 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,19 @@
+
+$('.inspired_slide_sections').slick({
+   prevArrow:'<i class="fa-solid fa-chevron-left banner_slick_arrow"></i>',
+  nextArrow:'<i class="fa-solid fa-chevron-right banner_slick_arrow"></i>',
+  slidesToShow: 2,
+  slidesToScroll: 1,
+ /* autoplay: true,
+  autoplaySpeed: 2000,*/
+});
+$('.celebration_slide_slick').slick({
+  prevArrow:'<i class="fa-solid fa-chevron-left celebration_slick_arrow"></i>',
+  nextArrow:'<i class="fa-solid fa-chevron-right celebration_slick_arrow"></i>',
+ // dots:true,
+ // dotsClass:'banner_slider_dots',
+ // fade: true,
+ // autoplay: true,
+ // autoplaySpeed:1000,
+ // speed:1000,
+});
